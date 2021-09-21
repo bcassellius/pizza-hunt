52b06881786e07370ff9c8753103904ef416f0ff
Bri <bhilliker@gmail.com>
2021-09-20 22:06:23 -0500

    setup the api endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+// Import all of the API routes /api/index.js (index.js implied)
+const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
+// Add prefix `/api` to all api routes imported from 'api' directory
+router.use('api', apiRoutes)
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
